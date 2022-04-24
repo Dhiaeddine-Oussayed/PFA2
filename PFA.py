11ec8179a2ae92e5efa3aba3726935a76438036f
Dhia_Oussayed <dhia.oussayed@gmail.com>
2022-04-24 23:05:31 +0100

    vectorizer saved

--- PFA.py
+++ PFA.py
@@ -3,11 +3,65 @@ import random
 from datetime import datetime
 import pyautogui
 import list_library
+from keras.models import Sequential
+from keras import Input
+from keras.layers import Dense
+import json
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
 import speech_recognition as sr
-from keras.models import load_model
-import prepare_data
+import pickle
 
-model = load_model('data')
+
+
+dataset = json.load(open('dataset.json'))
+
+df = pd.DataFrame(dataset, columns=['text', 'label'])
+df['text'] = df['text'].apply(lambda x: " ".join(x.lower() for x in x.split()))
+df['text'] = df['text'].str.replace('[^\w\s]', '')
+# df['text'] = df['text'].apply(lambda y: str(TextBlob(y).correct()))
+# df['text'] = df['text'].apply(lambda z: " ".join([Word(word).lemmatize() for word in z.split()]))
+
+
+dummies = ['label']
+dataframe = pd.get_dummies(df, columns=dummies)
+
+X = dataframe["text"]
+Y = dataframe.drop(['text'], axis=1)
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.05)
+
+x_train_list = x_train.tolist()
+x_test_list = x_test.tolist()
+Y_train = np.array(y_train)
+Y_test = np.array(y_test)
+
+# vectorizer = TfidfVectorizer()
+tfidf = pickle.load(open("tfidf.pkl", "rb"))
+A = tfidf.transform(x_train_list)
+B = tfidf.transform(x_test_list)
+
+A.sort_indices()
+B.sort_indices()
+number_of_classes = len(set(Y))
+
+model = Sequential()
+model.add(Input(shape=A.shape[1]))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(64, activation='relu'))
+
+model.add(Dense(number_of_classes, activation='softmax'))
+
+model.compile(optimizer="Adam", loss="categorical_crossentropy", metrics=["accuracy"])
+
+print(model.summary())
+
+history = model.fit(A, Y_train, validation_data=(B, Y_test), batch_size=30, epochs=10, verbose=1)
+
+
+# pickle.dump(tfidf, open("tfidf.pkl", "wb"))
 
 
 recording = sr.Recognizer()
@@ -19,11 +73,11 @@ with sr.Microphone() as source:
 print(command)
 
 test = [command]
-test = prepare_data.vectorizer.transform(test)
+test = tfidf.transform(test)
 test.sort_indices()
 prediction = model.predict(test)
 predicted_class = argmax(prediction)
-print(prepare_data.Y.columns[predicted_class])
+print(Y.columns[predicted_class])
 
 UserNameAsked = False
 
